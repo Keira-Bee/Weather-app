f93281858343928fd3d15f19c6c3f0b5c3217841
Keira-Bee <keira.a.burlinson@gmail.com>
2023-08-10 20:28:44 +0800

    Updated time

--- src/index.js
+++ src/index.js
@@ -25,6 +25,30 @@
 // let currentTime = new Date();
 // currentDay.innerHTML = formatDate(currentTime);
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function searchedCity(city) {
   let units = "metric";
   let apiKey = "667d9f573c8af4c33457be5d561a9148";
@@ -43,9 +67,11 @@ function showWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  document.querySelector("#precipitation").innerHTML = response.data.rain;
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
+  document.querySelector("#current-day").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
 function changeCity(event) {
